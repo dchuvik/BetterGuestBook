b68cbae1fc6faeeb841efda8c33df1487dc2ee7c
David Chuvik <dchuvik@gmail.com>
2023-02-14 15:58:44 -0500

    created GuestBook app, better than before

---
+++ BetterGuestBook/Program.cs
@@ -0,0 +1,68 @@
+﻿using GuestBookLibrary.Models;
+
+//Ask for guest info (first Name, last name, length of stay in days)
+//store guest info in list
+//loop thorugh list
+string moreGuests = string.Empty;
+bool isInt;
+
+List<GuestModel> guests = new List<GuestModel>();
+
+do
+{
+    GuestModel guest = new GuestModel();
+
+    GetGuestInfo(guest);
+
+    IntCheck(guest);
+
+    Console.Write("Any more guests (yes/no): ");
+    moreGuests = Console.ReadLine().ToLower();
+
+    guests.Add(guest);
+
+    Console.Clear();
+
+} while (moreGuests == "yes");
+
+
+foreach (GuestModel guest in guests)
+{
+	Console.WriteLine(guest.GuestInfo);
+}
+
+static void IntCheck(GuestModel guest)
+{
+    int days;
+    bool isInt;
+    do
+    {
+        isInt = int.TryParse(Console.ReadLine(), out days);
+        if (!isInt)
+        {
+            Console.Write("Invalid amount of days, please try again: ");
+        }
+        else
+        {
+            guest.LengthOfStayInDays = days;
+        }
+    } while (!isInt);
+}
+
+static void GetGuestInfo(GuestModel guest)
+{
+    
+    guest.FirstName = GetInfoFromConsole("What is your first name: ");
+    guest.LastName = GetInfoFromConsole("What is your last name: ");
+    Console.Write("How many days will you be staying: ");
+}
+
+static string GetInfoFromConsole(string message)
+{
+    string output = string.Empty;
+
+    Console.Write($"{message}");
+    output= Console.ReadLine();
+
+    return output;
+}
---
+++ GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookLibrary.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private int _lengthOfStayInDays;
+        public int LengthOfStayInDays
+        {
+            get
+            {
+                if (_lengthOfStayInDays >= 0 && _lengthOfStayInDays < 91)
+                {
+                    return _lengthOfStayInDays;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Out of range for length of stay.");
+                }
+               ;
+            }
+            set
+            {
+                _lengthOfStayInDays = value;
+            }
+        }
+
+
+        public string GuestInfo
+        {
+            get { return $"{FirstName} {LastName}: Staying for {LengthOfStayInDays} days."; }
+        }
+
+
+    }
+}
